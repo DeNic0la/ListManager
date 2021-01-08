35d93dde3b42257aea41a43e99899559e32c3cb5
DeNic0la <57986114+DeNic0la@users.noreply.github.com>
2021-01-08 16:09:45 +0000

    Comments

--- ListManager/main.cpp
+++ ListManager/main.cpp
@@ -49,18 +49,25 @@ void mapInt(ListElement* firstElement, int sortType);
     @autor (Laurin)
 */
 int main() {
+    // Anfangsmessage
     printf("Das Programm List-Manager wurde gestartet. Fuer Hilfe benuetzen sie den Command 'help'\n");
-    ListElement* pStartOfTheList = NULL;
+    ListElement* pStartOfTheList = NULL; // Variable Erstellen damit man in der gesamten main Funktion darauf zugreiffen kann
+    // Diese Schlaufe wird solag ausgeführt bis diese Variable true ist. Dann wird di Applikation beendet
     bool closeTheApplication = false;
     while (!closeTheApplication) {
+        // Eingabe vom Benutzer entgegennehmen und anhand dieser den Entsprechenden Code Teil im If/Else IF Konstrukt aufrufen
+        // Wenn die Eingabe nicht zugeordnet werden kann oder der Entsprechende Code Teil abgeschlossen wurde wird die erneut eine 
+        // Benutzereingabe eintgegengenommen
         char userInput[50] = { "empty" };
         fgets(userInput, 50, stdin);
         _strupr_s(userInput);
         if (strstr(userInput, "HELP")) {
+            // Auflistung der Befehle für den Benuzter
             printf("Moegliche commands:\ncreateList - Erstellt eine Liste\nprintList - gibt die Liste aus\ndeleteList - Loescht die Liste\nexit - Beendet die Applikation\n");
         }
         else if (strstr(userInput, "CREATELIST")) {
-            if (pStartOfTheList != NULL) {
+            // Listenerstellungsprozess
+            if (pStartOfTheList != NULL) { // Wenn eine Liste Existier wird gefragt ob diese Gelöscht werden soll
                 printf("Sie haben bereits eine erstellte Liste, moechten sie diese Loeschen [Y/N] ?\n");
                 char delListAnswer[50] = { "empty" };
                 fgets(delListAnswer, 50, stdin);
@@ -73,30 +80,35 @@ int main() {
                     continue;
                 }
             }
-
+            // Liste erstellen
             pStartOfTheList = createLinkedList(getIntFromUser("Wie viele elemente soll die Liste haben ?\n", false));
-            if (pStartOfTheList == NULL) {
+            if (pStartOfTheList == NULL) { // Liste war Fehlerhaft -> Message
                 printf("Fehler beim Erstellen der Liste\n");
                 continue;
             }
+            // Message
             printf("Liste wurde erstellt.\n");
             continue;
 
         }
         else if (strstr(userInput, "SORTLIST")) {
+            // Benuztereingabe zwischen 1 und 2 entgegennehmen für Sortieralgorythmuss
             int algorythm = 0;
             while (algorythm > 2 || algorythm < 1)
             {
                 algorythm = getIntFromUser("Mit welchem Algorythmus moechten sie die Liste sortieren ?\n1 = Mergesort (Nicola)\n2 = Quicksort (Laurin)\n", false);
             }
+            // Benuzereingabe für die Sortierart
             int sort = 0;
             while (sort > 4 || sort < 1)
             {
                 sort = getIntFromUser("Wie Soll sortiert werden ? \n1 = Bez - A-Z\n2 = Bez - Z-A\n3 = Preis - Aufsteigend\n4 = Preis - Absteigend\n", false);
             }
+            // Vor dem Start des Sortierprozess clock starten
             clock_t startZeit = clock();
+            // mappedInt wert in jedem Feld anhand des sort werts setzen, Dies ist auch teil des Sortiervorgangs
             mapInt(pStartOfTheList, sort);
-            switch (algorythm) {
+            switch (algorythm) { // Ruft die Entsprechende Funktion
             case 1:
                 N_MS_SortList(&pStartOfTheList);
                 break;
@@ -104,12 +116,13 @@ int main() {
                 L_QS_quickSort(pStartOfTheList);
                 break;
             }
+            // Zeitmessung beenden und ausgeben
             clock_t endZeit = clock();
             double dauer = ((double)endZeit - (double)startZeit) / (double)CLOCKS_PER_SEC;
             printf("Die Sortierung ist beendet und dauerte %.5lf Sekunden\n", dauer);
 
         }
-        else if (strstr(userInput, "PRINTLIST")) {
+        else if (strstr(userInput, "PRINTLIST")) {  // Gibt liste aus wenn vorhanden
             if (pStartOfTheList == NULL) {
                 printf("Keine liste vorhanden.\n");
                 continue;
@@ -117,7 +130,7 @@ int main() {
             printList(pStartOfTheList, getIntFromUser("Wie viele Elemente sollen auf einmal ausgegeben werden ? [-1 = alle] \n", true));
             continue;
         }
-        else if (strstr(userInput, "DELETELIST")) {
+        else if (strstr(userInput, "DELETELIST")) { // Liste Löschen wenn vorhanden
             if (pStartOfTheList == NULL) {
                 printf("Keine liste vorhanden.\n");
                 continue;
@@ -127,7 +140,7 @@ int main() {
             pStartOfTheList = NULL;
             continue;
         }
-        else if (strstr(userInput, "EXIT")) {
+        else if (strstr(userInput, "EXIT")) { // Beendet die Applikation
             return 0;
         }
     }
@@ -136,20 +149,24 @@ int main() {
 /*
     @autor Nicola
     The list is Built Back to Front
+    listSize ist die Anzahl Elemente welche die Liste haben soll
 */
 ListElement* createLinkedList(int listSize) {
-    ListElement* pPreviousElement = NULL;
-    for (int i = 0; i < listSize; i++) {
+    ListElement* pPreviousElement = NULL; // Für zugriff auserhalb der Schlaufe
+    for (int i = 0; i < listSize; i++) { // Iteration für Anzahl Elemente welche mitgegeben wurde
+        // Elemente "Erstellen"
         ListElement* pListElement = (ListElement*)malloc(sizeof(ListElement));
         DataElement* pDataElement = (DataElement*)malloc(sizeof(DataElement));
-        if (pListElement == NULL || pDataElement == NULL) {
+        if (pListElement == NULL || pDataElement == NULL) { // Wenn eines der Beiden NULL ist bedeudet dass das der Speicherzugriff Fehlgeschlagen ist
+            // Da pListElement nicht Existiert ist Hier der Letzte Punkt anwelchem auf die Liste zugegriffen werden Kann, die Liste wird
+            // Hier Gelöscht damit der Speicher wieder Freigegeben werden kann.
             deleteList(pPreviousElement);
             return NULL;
         }
-        fillData(pDataElement);
-        pListElement->pData = pDataElement;
-        pListElement->pNext = pPreviousElement;
-        if (pPreviousElement != NULL) {
+        fillData(pDataElement); // Feld wird befüllt
+        pListElement->pData = pDataElement; // Pointer auf Data Setzen
+        pListElement->pNext = pPreviousElement; // Pointer auf "Next" Setzen (Liste wird von Hinten nach vorne aufgebaut)
+        if (pPreviousElement != NULL) { // Double Linking
             pPreviousElement->pLast = pListElement;
         }
         pPreviousElement = pListElement;
@@ -160,6 +177,7 @@ ListElement* createLinkedList(int listSize) {
 
 /*
     @autor Nicola
+    Sort Type für sortierart, firstElement als Refferenz auf die Liste Für eine Umgekehrte Sortierung wird einfach der mappedInt vor dem Speichern invertiert
 */
 void mapInt(ListElement* firstElement, int sortType) {
 
@@ -173,6 +191,7 @@ void mapInt(ListElement* firstElement, int sortType) {
             4= Preis - Absteigend
         */
     case 1:
+        // In Jedem Element wird die Bezeichnung mit Bit-Shift in einen Integer wert Umgewandelt welcher Invertiert wird und im Data Element abgespeichert werden
         while (firstElement != NULL) {
             DataElement* element = firstElement->pData;
             element->mapped = -((element->Bez[0] << 16) + (element->Bez[1] << 8) + (element->Bez[2]));
@@ -180,6 +199,7 @@ void mapInt(ListElement* firstElement, int sortType) {
         }
         break;
     case 2:
+        // In Jedem Element wird die Bezeichnung mit Bit-Shift in einen Integer wert Umgewandelt und im Data Element abgespeichert werden
         while (firstElement != NULL) {
             DataElement* element = firstElement->pData;
             element->mapped = (element->Bez[0] << 16) + (element->Bez[1] << 8) + (element->Bez[2]);
@@ -187,6 +207,7 @@ void mapInt(ListElement* firstElement, int sortType) {
         }
         break;
     case 3:
+        // Der wert von Preis wird verlustfrei zu einem Int verwandelt und Invertiert
         while (firstElement != NULL) {
             DataElement* element = firstElement->pData;
             element->mapped = -((int)(element->Preis * 10));
@@ -194,6 +215,7 @@ void mapInt(ListElement* firstElement, int sortType) {
         }
         break;
     case 4:
+        // Der wert von Preis wird verlustfrei zu einem Int verwandelt
         while (firstElement != NULL) {
             DataElement* element = firstElement->pData;
             element->mapped = ((int)(element->Preis * 10));
@@ -246,25 +268,28 @@ void deleteList(ListElement* pToDelete) {
 }
 /*
     @autor Nicola
+    Ausgabe der Liste
 */
 void printList(ListElement* firstElement, int ElementsToPrintPerIteration) {
     bool exit = false;
     while (!exit)
     {
+        //Beginn ausgabe
         printf("Daten werden Ausgegeben:\n");
         printf("| ### Bez ### | ### Nummer ### |\n");
         printf("|-------------|----------------|\n");
-        for (int i = 0; i != ElementsToPrintPerIteration && firstElement->pNext != NULL; i++)
+        for (int i = 0; i != ElementsToPrintPerIteration && firstElement->pNext != NULL; i++) // For Schleife für Anzahl elemente 
         {
             firstElement = firstElement->pNext;
             DataElement* data = firstElement->pData;
-            //This Line is Preferences:
+            // Kosmetische Option:
             //printf("|-------------|----------------|\n");// This line can be Comment or code, visual changes only
             printf("|     %3s     |     %05.1f      |", data->Bez, data->Preis);
-            //printf("     %5i      |", data->mapped);// this line is for insight in the Mapped int
+            //printf("     %5i      |", data->mapped);// Zusatzfeature für Tiefer einsicht in die Sortierung
             printf("\n");
         }
         printf("|-------------|----------------|\n");
+        // Lässt den Benuzer sofern nicht die Komplette liste ausgegeben wurde eine weiter Iteration ausgeben
         char result[10] = { 'N' };
         if (firstElement->pNext == NULL) {
             printf("Es Wurden alle Elemente der Liste ausgegeben. Klicken sie eine Belibige Taste um zum Menu zurueckzukehren\n");
@@ -276,7 +301,7 @@ void printList(ListElement* firstElement, int ElementsToPrintPerIteration) {
             fgets(result, 10, stdin);
         }
 
-        exit = !isYes(result);
+        exit = !isYes(result); // Entscheidet ob die Schlaufe weiter läuft
     }
 }
 
@@ -295,6 +320,7 @@ bool isYes(char* inputstring) {
 
 /*
     @autor Nicola
+    Hilfsfunktion um einen Int-Wert vom Benuzer zu erhalten (Scanf hat wegen \n nicht funktioniert)
 */
 int getIntFromUser(const char* messageToUser, bool allowNegative) {
     while (true) {
@@ -376,14 +402,16 @@ void L_QS_quickSort(ListElement* head) {
 
 /*
     @autor Nicola
+    Main funktion des Mergesort algorythmus, Pointer Pointer auf den Anfang der Liste damit dieser auch verlegt werden kann
 */
 void N_MS_SortList(ListElement** firstElement) {
 
     ListElement* head = *firstElement;
-    if ((head == NULL) || (head->pNext == NULL)) {
-        return;
+    if ((head == NULL) || (head->pNext == NULL)) { // Falls eine Liste mit 0 oder 1 Element mitgegeben wurde ist diese Funktion fertig
+        return; // (Die Funktion wird Rekursiv aufgerufen)
     }
 
+    // Halbiere die Liste
     ListElement* a;
     ListElement* b;
     N_MS_Split(head, &a, &b);
@@ -391,10 +419,11 @@ void N_MS_SortList(ListElement** firstElement) {
     /*Recursive sort*/
     N_MS_SortList(&a);
     N_MS_SortList(&b);
-    *firstElement = N_MS_SortedMerge(a, b);
+    *firstElement = N_MS_SortedMerge(a, b); // "Rückgabe" übertrschreibt den Pointer
 }
 /*
     @autor Nicola
+    Halbiert die Liste
 */
 void N_MS_Split(ListElement* source,
     ListElement** start, ListElement** mid) {
@@ -402,7 +431,7 @@ void N_MS_Split(ListElement* source,
     ListElement* slow;
     slow = source;
     fast = source->pNext;
-
+    // Fast ist Doppelt so Schnell, wenn Fast mit der Liste fertig ist, ist Slow genau in der Mitte
     while (fast != NULL) {
         fast = fast->pNext;
         if (fast != NULL) {
@@ -410,6 +439,7 @@ void N_MS_Split(ListElement* source,
             fast = fast->pNext;
         }
     }
+    // "Returne die Werte" und Halbier die Liste durch "Löschen" eines Verweiss
     *start = source;
     *mid = slow->pNext;
     slow->pNext = NULL;
